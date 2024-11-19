ff6e57031f91213d13ed46da7fc361d9d3eb4cf9
claragrpereira <claragrpereira@gmail.com>
2024-11-19 16:05:18 +0000

    quase a correr

---
+++ src/data_loader2.py
@@ -0,0 +1,45 @@
+import torch
+from torch.utils.data import Dataset, DataLoader
+from sklearn.model_selection import train_test_split 
+
+class TensorDataset(Dataset):
+    def __init__(self, features, labels):
+        """
+        Initialize the TensorDataset with features and labels.
+
+        Parameters:
+            data_path (str): Path to the .pt file containing preprocessed data.
+                            The file is expected to contain a tuple (features, labels).
+        """
+        self.features = features
+        self.labels = labels
+
+    def __len__(self):
+        return len(self.labels)
+
+    def __getitem__(self, idx):
+        return self.features[idx], self.labels[idx]
+
+
+def dataloader(path_tiny, path_r):
+    # Load datasets
+    data_tiny = torch.load(path_tiny)
+    data_r = torch.load(path_r)
+
+    # Split tiny in train, val, test
+    train_x, test_tiny_x, train_y, test_tiny_y = train_test_split(data_tiny[0], data_tiny[1], test_size = 0.1, stratify = data_tiny[1])
+    train_x, val_x, train_y, val_y = train_test_split(train_x, train_y, test_size = 0.1, stratify = train_y)
+
+    # Create datasets
+    train_dataset = TensorDataset(train_x, train_y)
+    val_dataset = TensorDataset(val_x, val_y)
+    test_tiny = TensorDataset(test_tiny_x, test_tiny_y)
+    r_dataset = TensorDataset(data_r[0], data_r[1])
+
+    # Create DataLoaders
+    train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
+    val_loader = DataLoader(val_dataset, batch_size=32, shuffle=True)
+    test_loader_tiny = DataLoader(test_tiny, batch_size=32, shuffle=False)
+    test_loader_r = DataLoader(r_dataset, batch_size=32, shuffle=False)
+
+    return train_loader, val_loader, test_loader_tiny, test_loader_r
--- src/eval_tta.py
+++ src/eval_tta.py
@@ -1,12 +1,19 @@
 
 import torchvision.models as models
 from train import PyTorchTrainer
+from data_loader2 import dataloader
+from torch import nn, optim
 #mesmo nome que no https://github.com/fra31/evaluating-adaptive-test-time-defenses/tree/master
+
+path_tiny = "/Users/clarapereira/Desktop/Uni/Ano_5/PIC/datasets/test/train.pt" #diretoria para o tensor
+path_r = "/Users/clarapereira/Desktop/Uni/Ano_5/PIC/datasets/test/test.pt" #diretoria para o tensor
 
 models_to_train = [
     {"name": "mobilenet_v3", "model": models.mobilenet_v3_small, "params": {"lr": 0.001}} #,
     #{"name": "resnet18", "model": models.resnet18, "params": {"lr": 0.0001}},
 ]
+
+train_loader, val_loader, test_loader_tiny, test_loader_r = dataloader(path_tiny, path_r)
 
 for config in models_to_train:
     model_name = f"{config['name']}_lr{config['params']['lr']}"
@@ -14,13 +21,13 @@ for config in models_to_train:
         model=config["model"](pretrained=True),
         train_loader=train_loader,
         val_loader=val_loader,
-        test_loader=test_loader,
+        test_loader=test_loader_r, #por enquanto
         criterion=nn.CrossEntropyLoss(),
         optimizer=optim.Adam(config["model"](pretrained=True).parameters(), lr=config["params"]["lr"]),
     )
 
     trainer.train(num_epochs=10, model_name=model_name, save_dir="project_root")
     trainer.save_predictions(val_loader, f"project_root/results/{model_name}/val_predictions.csv", "val")
-    trainer.save_predictions(test_loader, f"project_root/results/{model_name}/test_predictions.csv", "test")
+    trainer.save_predictions(test_loader_r, f"project_root/results/{model_name}/test_predictions.csv", "test")
     trainer.save_training_info(f"project_root/results/{model_name}/training_info.json")
     trainer.save_plots(f"project_root/reports/figures/{model_name}_loss_accuracy.png")
